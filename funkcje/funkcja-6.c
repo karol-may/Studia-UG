d398ecca89aba64a0c914720328aa961b8f3314a
Karol Gontarski <karol.gontarski@gmail.com>
2011-11-27 13:08:24 +0100

    Problem z rand() - rozwiązany

--- funkcje/funkcja-6.c
+++ funkcje/funkcja-6.c
@@ -1,49 +1,55 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 
-int random_int() {
+int random_int()
+{
 
-	 /* initialize random seed: */
-	 srand ( time(NULL) );
-
-	/* generate secret number: */
-	return rand() % 11;
-
+    /* generate secret number: */
+    return rand() % 11;
 
 }
 
- int tab[5][2];   
- 
-void generate_tasks(int qty){
-	
+int tab[5][3];
 
-	int i;
-	for (i=0; i<qty; i++){
+void generate_tasks(int qty)
+{
+
+
+    int i;
+    for (i = 0; i < qty; i++) {
 	tab[i][0] = random_int();
 	tab[i][1] = random_int();
 	tab[i][2] = tab[i][0] * tab[i][1];
-	}
+    }
 }
 
 
 
 int main()
 {
-	
-	
-	
-	int ans;
+    int ans;
+
+
+    /* initialize random seed: */
+    srand(time(NULL));
+
+    int qty = sizeof(tab) / sizeof(tab[0]);
     
-    int qty = sizeof tab;
-	generate_tasks(qty);
-	printf("Hi! There is few questions for you: \n ");
-	int i;
-	for (i=0; i<qty; i++){
-		printf("\n \t %d * %d = ", tab[i][0],tab[i][1] );
-		scanf("%d",  &ans);
-		if (tab[i][2] == ans) { printf("OK!"); } else { printf("SORRY! It's %d", tab[i][2]); }
-		
-	}
-	
+    generate_tasks(qty);
+    printf("Hi! There are few questions for you: \n ");
+    int i;
+    for (i = 0; i < qty; i++) {
+	printf("\n \t %d * %d = ", tab[i][0], tab[i][1]);
+	scanf("%d", &ans);
+		if (tab[i][2] == ans) {
+			printf("OK!");
+		} else {
+			printf("SORRY! It's %d\n", tab[i][2]);
+		}
+
+    }
+return 0;
 }
