6f5fcaa34eb0263ed0e0962ae6e55c66a7123600
Karol Gontarski <karol.gontarski@gmail.com>
2011-12-13 04:09:45 +0100

    zadanie 1

--- elementarz 1/elem-1.c
+++ elementarz 1/elem-1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main() {
-
-	   
-   return 0;
+	int n1=5+3*8/2-3;
+	int n2=2%2+2*2-2/2;
+    int n3=2*4*(5+9/2);
+   
+   	printf(n1=%d\tn2=%d\tn3=%d, n1,n2,n3);   
+   
+   	return 0;
 }
