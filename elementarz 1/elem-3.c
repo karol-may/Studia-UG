bbc44dad9f107f685183e230e487fbb241935bb7
Karol Gontarski <karol.gontarski@gmail.com>
2011-12-13 04:00:38 +0100

    poprawki do  zadań 1, 2 i 3

--- elementarz 1/elem-3.c
+++ elementarz 1/elem-3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main() {
    int tabela[] = {1, 2, 4, 6, 12};
-   int i;
-   for (i = sizeof(tabela)/sizeof(int)-1;i>=0;i--){
+   int i = sizeof(tabela)/sizeof(int);
+   while (i>0){
+   	i--;
 	printf("%d ",tabela[i]);  
    }
    
